9e9de902b42ccbe748fe1ddfd08d15d59fa6522d
Andre Franklin <Andre Franklin@192.168.0.6>
2018-04-13 19:01:30 -0300

    Mudanças nos codigo do DAO e ConexaoMySQL do SiSVendas.

--- src/DAO/DaoProdutos.java
+++ src/DAO/DaoProdutos.java
@@ -1,16 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ SiSVendas
  */
 package DAO;
 import Conexao.ConexaoMySql;
+import modelo.ModelProdutos;
 /*
 @author Andre Franklin
 */
 
 public class DaoProdutos extends ConexaoMySql {
     
-    
+    public int salvarProdutosDao(ModelProdutos pModelProdutos){
+        try{
+            this.conectar();
+            return this.insertSQL("INSERT INTO tbl_produto("
+                    + "nome_prod,"
+                    + " valor_prod,"
+                    + " estoque_prod"
+                    + ")VALUES ("
+                    + "'" + pModelProdutos.getNome_prod()+ "',"
+                    + "'" + pModelProdutos.getValor_prod()+ "',"
+                    + "'" + pModelProdutos.getEstoque_prod()+ "'"
+                    );            
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }finally{
+            this.fecharConexao();
+        }
+        
+    }    
     
 }
