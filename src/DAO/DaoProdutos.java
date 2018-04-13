60a65e58af4a17e1afcb6181c311ba34dbf0ddfa
Andre Franklin <Andre Franklin@192.168.0.6>
2018-04-13 20:22:04 -0300

    Mudanças no codigo do DAO do SiSVendas.

--- src/DAO/DaoProdutos.java
+++ src/DAO/DaoProdutos.java
@@ -2,33 +2,81 @@
  SiSVendas
  */
 package DAO;
+
 import Conexao.ConexaoMySql;
 import modelo.ModelProdutos;
+
 /*
 @author Andre Franklin
-*/
-
+ */
 public class DaoProdutos extends ConexaoMySql {
-    
-    public int salvarProdutosDao(ModelProdutos pModelProdutos){
-        try{
+
+    /**
+     * CADASTRAR UM PRODUTO NO BANCO
+     *
+     * @param pModelProdutos
+     * @return
+     */
+    public int salvarProdutosDAO(ModelProdutos pModelProdutos) {
+        try {
             this.conectar();
             return this.insertSQL("INSERT INTO tbl_produto("
                     + "nome_prod,"
                     + " valor_prod,"
                     + " estoque_prod"
                     + ")VALUES ("
-                    + "'" + pModelProdutos.getNome_prod()+ "',"
-                    + "'" + pModelProdutos.getValor_prod()+ "',"
-                    + "'" + pModelProdutos.getEstoque_prod()+ "'"
-                    );            
-        }catch(Exception e){
+                    + "'" + pModelProdutos.getNome_prod() + "',"
+                    + "'" + pModelProdutos.getValor_prod() + "',"
+                    + "'" + pModelProdutos.getEstoque_prod() + "'"
+            );
+        } catch (Exception e) {
             e.printStackTrace();
             return 0;
-        }finally{
+        } finally {
             this.fecharConexao();
         }
-        
-    }    
-    
+
+    }
+
+    /**
+     * EXCLUIR UM PRODUTO DO BANCO
+     *
+     * @param pIdProduto
+     * @return boolean
+     */
+    public boolean excluirProdutoDAO(int pIdProduto) {
+        try {
+            this.conectar();
+            return this.executarUpdateDeleteSQL(
+                    "DELETE FROM tbl_produto WHERE pk_id_produto = '" + pIdProduto + "'"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            this.fecharConexao();
+        }
+    }
+    /**
+     * Alterar dados do produtos
+     * @param pModelProdutos
+     * @return boolean
+     */
+    public boolean alterarProdutoDAO(ModelProdutos pModelProdutos) {
+        try {
+            this.conectar();
+            return this.executarUpdateDeleteSQL(
+                    "UPDATE tbl_produto SET"
+                    + "nome_prod = '" + pModelProdutos.getNome_prod() + "',"
+                    + " valor_prod = '" + pModelProdutos.getValor_prod() + "',"
+                    + " estoque_prod= '" + pModelProdutos.getEstoque_prod() + "',"
+                    + "WHERE pk_id_produto = '" + pModelProdutos.getId_produtos() + "'"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            this.fecharConexao();
+        }
+    }
 }
